51859c14a8f8fccf4d23e95c99c2f0c54283c62e
Gommash(Anle1802) <a.lekedal@gmail.com>
2022-05-16 23:32:28 +0200

    Update ImageManipulator.java

--- src/imagemanipulator/ImageManipulator.java
+++ src/imagemanipulator/ImageManipulator.java
@@ -1,7 +1,9 @@
 package imagemanipulator;
 
+import imagemanipulator_macros.Clahe;
 import imagemanipulator_macros.GaussianBlur;
 import imagemanipulator_macros.RandomCropping;
+import imagemanipulator_macros.RichardsonLucy;
 import imagemanipulator_macros.RollingBall;
 
 public class ImageManipulator {
@@ -10,12 +12,13 @@ public class ImageManipulator {
 		//RandomCropping rc = new RandomCropping(144,144,24,false);
 		//rc.execute();
 		// Add second image enhancement filter on top
-		GaussianBlur gb = new GaussianBlur(3,3,false);
-		gb.execute();
+		//GaussianBlur gb = new GaussianBlur(3,3,false);
+		//gb.execute();
 		// Add third image enhancement filter on top
-		RollingBall rb = new RollingBall(100,true,false,true);
-		rb.execute();
-		
+		//RollingBall rb = new RollingBall(70.5f,true,true,false);
+		//rb.execute();
+		RichardsonLucy r = new RichardsonLucy(3,false);
+		r.execute();
 	}
 
 }
